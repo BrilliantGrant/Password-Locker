1acdaec94bd1413545436be3797d790bdd131d12
brilliant kaimba <briegrant416@gmail.com>
2018-01-19 11:40:46 +0300

    Add def test_Credential_instance to credential_test.py

--- credentials.py
+++ credentials.py
@@ -11,3 +11,5 @@ credentials_list = []
 
 
 
+if __name__ == '__main__':
+    unittest.main()
--- credentials_test.py
+++ credentials_test.py
@@ -5,6 +5,12 @@ class TestCredentials(unittest.TestCase):
 	def setup(self):
 	self.new_credentials = Credentials("Instagram","12345")
 
+	def test_credentials_instance(self):
+		self.assertEqual(self.new_credentials.account_name,"Instagram")
+		self.assertEqual(self.new_credentials.account_password, "12345")
+
+	
+
 
 
 
