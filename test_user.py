052a1cfe9ea351e1d9e435345e74c900bcb3f1ac
brilliant kaimba <briegrant416@gmail.com>
2018-01-19 12:55:56 +0300

    Add Docstrings  to test_user.py

--- test_user.py
+++ test_user.py
@@ -1,7 +1,11 @@
-import unittest
-from user import User
+import unittest #Importing the unittest module
+from user import User #Import the user class"""
 
 class Testuser(unittest.TestCase):
+
+	"""Test class that defines test cases for the contact behaviours.
+	Args:
+	unittest.TestCase: TestCase class that helps in creating test cases"""
 	
 	def setUp(self):
 		"""Set up method to run before eaach test cases"""
