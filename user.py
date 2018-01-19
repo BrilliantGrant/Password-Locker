adfaeb878bad5ee72096511fac1a680692bf4375
brilliant kaimba <briegrant416@gmail.com>
2018-01-19 08:10:59 +0300

    make changes in test_user.py

--- user.py
+++ user.py
@@ -1,15 +1,13 @@
 class user:
+    """Create class for users"""
 
-	"""Create class for users"""
+    def __init__(self, user_name, password):
+        self.user_name = user_name
+        self.password = password
+    user_list = []
 
-	def__init__(self, user_name, password):
-		self.user_name = user_name
-		self.password = password
-	user_list = []
-
-	def save_user(self):
-		self.user_list.append(self)
+    
 
 
-if__name__=='__main__':
-	main()
+if __name__ == '__main__':
+    main()
