c7f9ce48017a3f28b45985693004dae7a0e88cea
brilliant kaimba <briegrant416@gmail.com>
2018-01-19 07:48:09 +0300

    Add unittest.main function in test_user.py

--- test_user.py
+++ test_user.py
@@ -1,2 +1,12 @@
 import unittest
 from user import user
+
+class Testuser(unittest.TestCase):
+	
+	def setup(self):
+		self.new = user("Newuser","12345")
+
+
+
+if __name__ == '__main__':
+    unittest.main()
