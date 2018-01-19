1f0c4e83bf45a9e43115bf49b8b108a46bbf7d1e
brilliant kaimba <briegrant416@gmail.com>
2018-01-19 14:49:18 +0300

    Make changes to credentials_user.py

--- credentials.py
+++ credentials.py
@@ -1,51 +1,51 @@
 class Credentials:
 
 
-def__init__(self,account_name, account_password):
-	self.account_name = account_name
-	self.account_password = account_password
+	def __init__ (self,account_name, account_password):
+		self.account_name = account_name
+		self.account_password = account_password
 
-credentials_list = []
+		credentials_list = []
 
-def save_credentials(self):
-	self.credentials_list.append(self)
+	def save_credentials(self):
+	    self.credentials_list.append(self)
+	    
+	def delete_credentials(self):
+		"""delete _contact method deletes a saved contact from contact_list"""
+		Credentials.credentials_list.remove(self)
 
-def delete_credentials(self):
-	"""delete _contact method deletes a saved contact from contact_list"""
-	Credentials.credentials_list.remove(self)
+	@classmethod
 
-@classmethod
-    def find_by_name(cls, account_name):
-    	"""Method that atkes in a number and returns that matches that number.
+	def find_by_name(cls, account_name):
+		"""Method that atkes in a number and returns that matches that number.
 
-    	Args: 
+		Args: 
 
-    		number: Phone number to search for
+		account_name: user_name to search for
 
-    	Returns:
-    	 	Contact of person that matches the number"""
+		Returns:
+		user name that matches the number"""
+		for credential in cls.credentials_list:
+			if credential.account_name == account_name:
+			    return credential
 
-        for credential in cls.credentials_list:
-            if credential.account_name == account_name:
-                return credential
+	@classmethod
+	def credential_exists(cls, name):
+		"""Method to check whether a credential exists
+		Args:
+		name: name of account to search whether it exists
+		boolean: True or False depending if the contatc exists
+		"""
 
- @classmethod
-    def credential_exists(cls, name):
-        """Method to check whether a credential exists
-        Args:
-        name: name of account to search whether it exists
-        boolean: True or False depending if the contatc exists
-        """
+		for credential in cls.credentials_list:
+			if credential.account_name == name:
+			    return True
+		return False
 
-        for credential in cls.credentials_list:
-            if credential.account_name == name:
-                return True
-        return False
-
- @classmethod
-    def display_credentials(cls):
-        """Method which displays all current credentials"""
-        return cls.credentials_list
+	@classmethod
+	def display_credentials(cls):
+	    """Method which displays all current credentials"""
+	    return cls.credentials_list
 
 
 
--- credentials_test.py
+++ credentials_test.py
@@ -1,45 +1,54 @@
 import unittest
 from credentials import Credentials
 
+
 class TestCredentials(unittest.TestCase): 
+
+	"""Test class that defines test cases for the contact behaviours.
+	Args:
+	unittest.TestCase: TestCase class that helps in creating test cases"""
 	def setup(self):
-	self.new_credentials = Credentials("Instagram","567")
+		"""Set up method to run before eaach test cases"""
+		self.new_credentials = Credentials("Instagram","567")
 
 	def test_credentials_instance(self):
 		self.assertEqual(self.new_credentials.account_name,"Instagram")
 		self.assertEqual(self.new_credentials.account_password, "567")
 
 	def test_save_credentials(self):
-        """Method that tests whether the new credential created has been saved"""
-        self.new_credentials.save_credentials()
-        self.assertEqual(len(Credentials.credentials_list), 1)
+		"""Method that tests whether the new credential created has been saved"""
+		self.new_credentials.save_credentials()
+		self.assertEqual(len(Credentials.credentials_list), 1)
 
-    def test_save_multiple_credentials(self):
-        """Method that saves multiple credentials to credentials_list"""
-        self.new_credentials.save_credentials()
-        new_test_credential = Credentials("Tumblr", "5678")
-        new_test_credential.save_credentials()
-        self.assertEqual(len(Credentials.credentials_list), 2)
+	def test_save_multiple_credentials(self):
+		"""Method that saves multiple credentials to credentials_list"""
+		self.new_credentials.save_credentials()
+		new_test_credential = Credentials("Tumblr", "5678")
+		new_test_credential.save_credentials()
+		self.assertEqual(len(Credentials.credentials_list), 2)
 
-    def tearDown(self):
-        Credentials.credentials_list = []
+	def tearDown(self):
+		"""tearDown method that does clean up after each test case has run"""
+		Credentials.credentials_list = []
 
-    def test_find_credential_by_name(self):
-        self.new_credentials.save_credentials()
-        new_test_credential = Credentials("Tumblr", "5678")
-        new_test_credential.save_credentials()
+	def test_find_credential_by_name(self):
+		'''
+		Method that takes in a name and returns a user that matches that namer.
+		Returns :
+		Credentials of person that matches the name.
+		'''
+		self.new_credentials.save_credentials()
+		new_test_credential = Credentials("Tumblr", "5678")
+		new_test_credential.save_credentials()
 
-        found_credential = Credentials.find_by_name("Tumblr")
+		found_credential = Credentials.find_by_name("Tumblr")
 
-        self.assertEqual(found_credential.account_name, new_test_credential.account_name)
+		self.assertEqual(found_credential.account_name, new_test_credential.account_name)
 
-    def test_display_all_credentials(self):
-        """TestCase to test whether all contacts can be displayed"""
-        self.assertEqual(Credentials.display_credentials(), Credentials.credentials_list)
-
-
-
-
+	def test_display_all_credentials(self):
+		"""TestCase to test whether all users can be displayed"""
+		self.assertEqual(Credentials.display_credentials(), Credentials.credentials_list)
+		
 
 
 
